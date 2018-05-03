ec4651d9ed3b05613667c908459551130cdb0b0d
chao <754133562@qq.com>
2018-05-03 12:04:40 +0800

    增加了创建课程功能 个人信息页面 TODO ： （=。=） 加入课程 巴拉巴拉

--- app/src/main/java/cn/adeerlongneck/app/smartschoolapp/Activity/AddCourseActivity.java
+++ app/src/main/java/cn/adeerlongneck/app/smartschoolapp/Activity/AddCourseActivity.java
@@ -1,15 +1,58 @@
 package cn.adeerlongneck.app.smartschoolapp.Activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import cn.adeerlongneck.app.smartschoolapp.MyApplication;
+import cn.adeerlongneck.app.smartschoolapp.Presenster.AddCoursePresent;
 import cn.adeerlongneck.app.smartschoolapp.R;
+import cn.adeerlongneck.app.smartschoolapp.View.AddCourseView;
 
-public class AddCourseActivity extends AppCompatActivity {
-
+public class AddCourseActivity extends AppCompatActivity implements AddCourseView {
+    EditText editText;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_course);
+        MyApplication application = (MyApplication) getApplicationContext();
+        final String stuid = application.getStuid();
+
+        editText = (EditText) findViewById(R.id.add);
+        button = (Button) findViewById(R.id.bt_add);
+        final AddCoursePresent addCoursePresent = new AddCoursePresent(this);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addCoursePresent.add(editText.getText().toString(), stuid);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void success() {
+        Toast.makeText(this, "添加成功", Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    @Override
+    public void fail() {
+        Toast.makeText(this, "添加失败", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showDialog() {
+
+    }
+
+    @Override
+    public void hideDialog() {
+
     }
 }
--- app/src/main/java/cn/adeerlongneck/app/smartschoolapp/Activity/SignActivity.java
+++ app/src/main/java/cn/adeerlongneck/app/smartschoolapp/Activity/SignActivity.java
@@ -1,15 +1,39 @@
 package cn.adeerlongneck.app.smartschoolapp.Activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import cn.adeerlongneck.app.smartschoolapp.R;
 
 public class SignActivity extends AppCompatActivity {
 
+    Button bt_addCourse;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign);
+        ini();
     }
+
+    public void ini() {
+        bt_addCourse = (Button) findViewById(R.id.addcourse);
+        bt_addCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignActivity.this, AddCourseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+
+
+
+
 }
